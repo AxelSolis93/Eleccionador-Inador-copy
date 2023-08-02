feba6f67f96d48d0a3f6160d0a04132bd0b34eda
AxelSolis93 <asolisz@unsa.edu.pe>
2023-08-02 00:25:38 -0500

    Update
    No more sonar errors

--- VOTER/VOTER.js
+++ VOTER/VOTER.js
@@ -1,20 +1,40 @@
-class VOTER {
-    constructor() {
-    }
+import React from 'react';
 
-    V_ID;
-    V_Password;
-    V_Name;
+class Voter extends React.Component {
+  constructor(props) {
+    super(props);
 
-    castVote() {
-        // TODO implement here
-    }
+    this.state = {
+      V_ID: null,
+      V_Password: null,
+      V_Name: null,
+    };
+  }
 
-    operation1() {
-        // TODO implement here
-    }
+  castVote = () => {
+    // TODO implement here
+  }
 
-    viewResult() {
-        // TODO implement here
-    }
+  operation1 = () => {
+    // TODO implement here
+  }
+
+  viewResult = () => {
+    // TODO implement here
+  }
+
+  render() {
+    return (
+      <div>
+    
+        <p>Voter ID: {this.state.V_ID}</p>
+        <p>Voter Name: {this.state.V_Name}</p>
+        <button onClick={this.castVote}>Cast Vote</button>
+        <button onClick={this.operation1}>Operation1</button>
+        <button onClick={this.viewResult}>View Result</button>
+      </div>
+    );
+  }
 }
+
+export default Voter;
--- Voter PROFILE/Voter PROFILE.js
+++ Voter PROFILE/Voter PROFILE.js
@@ -1,21 +1,42 @@
-class VoterPROFILE {
-    constructor() {
-    }
+import React from 'react';
 
-    V_ID;
-    V_Password;
-    V_Name;
-    V_Information;
+class VoterPROFILE extends React.Component {
+  constructor(props) {
+    super(props);
 
-    castVote() {
-        // TODO implement here
-    }
+    this.state = {
+      V_ID: null,
+      V_Password: null,
+      V_Name: null,
+      V_Information: null,
+    };
+  }
 
-    viewResult() {
-        // TODO implement here
-    }
+  castVote = () => {
+    // TODO implement here
+  }
 
-    operation3() {
-        // TODO implement here
-    }
+  viewResult = () => {
+    // TODO implement here
+  }
+
+  operation3 = () => {
+    // TODO implement here
+  }
+
+  render() {
+    return (
+      <div>
+
+        <p>Voter ID: {this.state.V_ID}</p>
+        <p>Voter Name: {this.state.V_Name}</p>
+        <p>Voter Information: {this.state.V_Information}</p>
+        <button onClick={this.castVote}>Cast Vote</button>
+        <button onClick={this.viewResult}>View Result</button>
+        <button onClick={this.operation3}>Operation 3</button>
+      </div>
+    );
+  }
 }
+
+export default VoterPROFILE;
